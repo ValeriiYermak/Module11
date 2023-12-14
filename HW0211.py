b1624f421d0f5506d55764d6ff70ea1374063faf
YOUR NAME <your email address>
2023-12-14 17:10:19 +0200

    first commit

---
+++ HW0211.py
@@ -0,0 +1,37 @@
+'''У класу Point через конструктор __init__ оголошено два атрибути: координати x та y. 
+Приховати доступ до них з допомогою подвійного підкреслення: __x та __y
+
+Реалізуйте для класу Point механізми setter та getter до атрибутів __x та __y за допомогою 
+декораторів property та setter.
+
+Приклад:
+
+point = Point(5, 10)
+
+print(point.x)  # 5
+print(point.y)  # 10'''
+
+
+
+class Point:
+    def __init__(self, x, y):
+        self.__x = None
+        self.__y = None
+        self.x = x
+        self.y = y
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        self.__x = x
+        
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        self.__y = y
